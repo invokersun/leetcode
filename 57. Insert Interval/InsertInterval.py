41f9bc5aaad681126dd978142a2bc579780f8db7
invokersun <xhst2014@163.com>
2017-11-15 10:36:42 +0800

    leetcode

---
+++ 57. Insert Interval/InsertInterval.py
@@ -0,0 +1,27 @@
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[Interval]
+        :type newInterval: Interval
+        :rtype: List[Interval]
+        """
+        intervals.append(newInterval)
+        intervals.sort(key = lambda x:x.start)
+        length = len(intervals)
+        res=[]
+        for i in range(length):
+        	if res==[]:
+        		res.append(intervals[i])
+        	else:
+        		size = len(res)
+        		if res[size-1].start<=intervals[i].start<=res[size-1].end:
+        			res[size-1].end = max(intervals[i].end, res[size-1].end)
+        		else:
+        			res.append(intervals[i])
+        return res
---
+++ 58. Length of Last Word/LengthofLastWord.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return len(s.rstrip().split(' ')[-1])
+print Solution().lengthOfLastWord('Hello')
